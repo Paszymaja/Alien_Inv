003d8537988d3ff4a0539a61d3ec4379ac416948
Paszymaja <sprzepiora@hotmail.co.uk>
2018-06-07 14:40:56 +0200

    Shoting Bullets

--- Main.py
+++ Main.py
@@ -3,6 +3,7 @@ import pygame
 from settings import Settings
 from ship import Ship
 from background import Background
+from pygame.sprite import Group
 import game_functions as gf
 
 
@@ -15,12 +16,20 @@ def run_game():
     pygame.display.set_caption('Alien Inv')
     screen_y = 0
     background = Background(screen_settings.bg_image, [0, 0])
+    bullets = Group()
 
     while True:  # Game Loop
-        gf.check_events(ship)
+        gf.check_events(screen_settings, screen, ship, bullets)
         ship.update()
-        gf.update_screen(screen_settings, screen, ship, screen_y, background)
+        bullets.update()
+        gf.update_screen(screen_settings, screen, ship, screen_y, background, bullets)
         screen_y += screen_settings.bg_speed
+
+        for bullet in bullets.copy():
+            if bullet.rect.bottom <= 0:
+                bullets.remove(bullet)
+            print(len(bullets))
+
 
 
 run_game()
--- game_functions.py
+++ game_functions.py
@@ -1,28 +1,31 @@
 import sys
 import pygame
+from bullet import Bullet
 
 
-def check_events(ship):
+def check_events(settings, screen, ship, bullets):
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             sys.exit()
         elif event.type == pygame.KEYDOWN:
-            check_keydown_events(event, ship)
+            check_keydown_events(event, settings, screen, ship, bullets)
         elif event.type == pygame.KEYUP:
             check_keyup_events(event, ship)
 
 
-def update_screen(screen_settings, screen, ship, screen_y, background):
+def update_screen(screen_settings, screen, ship, screen_y, background, bullets):
     screen.fill(screen_settings.bg_color)
     rel_y = screen_y % background.height
     screen.blit(background.image, (0, rel_y - background.height))
     if rel_y < screen_settings.screen_height:
         screen.blit(background.image, (0, rel_y))
+    for bullet in bullets.sprites():
+        bullet.draw_bullet()
     ship.blitme()
     pygame.display.update()
 
 
-def check_keydown_events(event, ship):
+def check_keydown_events(event, settings, screen, ship, bullets):
     if event.key == pygame.K_RIGHT:
         ship.moving_right = True
     elif event.key == pygame.K_LEFT:
@@ -31,6 +34,9 @@ def check_keydown_events(event, ship):
         ship.moving_up = True
     elif event.key == pygame.K_DOWN:
         ship.moving_down = True
+    elif event.key == pygame.K_SPACE:
+        new_bullet = Bullet(settings, screen, ship)
+        bullets.add(new_bullet)
 
 
 def check_keyup_events(event, ship):
